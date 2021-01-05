952e2d6340509d69990061402e1c36f42252a1ce
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2021-01-04 22:46:16 -0600

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -13,9 +13,9 @@ var gree4 = 'Good evening, ';
 
 // Define the hours of the greetings
 if (hour >= 22 || hour < 5) {
-    document.getElementById('greetings').innerText = gree1 + name + '!';
+    document.getElementById('greetings').innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
-    document.getElementById('greetings').innerText = gree2 + name + '!';
+    document.getElementById('greetings').innerText = gree2 + name;
 } else if (hour >= 12 && hour < 17) {
     document.getElementById('greetings').innerText = gree3 + name;
 } else  {
