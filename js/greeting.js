7b4d8445a26f1c558f07b2bdb53302934b24dd2a
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2021-01-04 22:36:44 -0600

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -6,15 +6,13 @@ var hour = today.getHours();
 var name = 'Gerrit';
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
+var gree1 = 'Go to sleep ';
+var gree2 = 'Good morning ';
 var gree3 = 'Good afternoon ';
 var gree4 = 'Good evening ';
-var gree5 = 'Good evening ';
-var gree6 = 'Good evening ';
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
+if (hour >= 22 && hour < 5) {
     document.getElementById('greetings').innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
     document.getElementById('greetings').innerText = gree2 + name;
